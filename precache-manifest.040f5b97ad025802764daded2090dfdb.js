3ade9da19843bd302e59733825f291b4cba09d0a
ja10ns0 <alonsofernandezjaime@gmail.com>
2020-01-31 12:23:11 +0100

    publish

---
+++ precache-manifest.040f5b97ad025802764daded2090dfdb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ca9bf745887bb52d40e208927c68926",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9879fce9c42daf78d8d2",
+    "url": "./static/css/2.5bc09813.chunk.css"
+  },
+  {
+    "revision": "9879fce9c42daf78d8d2",
+    "url": "./static/js/2.3b8def20.chunk.js"
+  },
+  {
+    "revision": "3237149ff97fe7054b06",
+    "url": "./static/js/main.5da28c6e.chunk.js"
+  },
+  {
+    "revision": "c7c4ef385efd2e7c5be5",
+    "url": "./static/js/runtime-main.eb47b225.js"
+  },
+  {
+    "revision": "923829e978ee9f7c7d39c5477ab53b7a",
+    "url": "./static/media/iconArrowRight36x36.923829e9.svg"
+  },
+  {
+    "revision": "de1872eea8a2930464ed4e2620bcfe0c",
+    "url": "./static/media/iconCheckCircle24x24.de1872ee.svg"
+  },
+  {
+    "revision": "9ae9f979708070b55cebab6164dc8571",
+    "url": "./static/media/iconMenuToClose36x36.9ae9f979.svg"
+  }
+]);
